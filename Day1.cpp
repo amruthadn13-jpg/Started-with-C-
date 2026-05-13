4227ca1df90f559e4f6857dd41f847c3c2ce7377
Amrutha D N <amruthadn13@gmail.com>
2026-05-13 21:16:29 +0530

    Create Day1.cpp

---
+++ Day1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "==================================" << endl;
+    cout << "       C++ Programming Journey    " << endl;
+    cout << "==================================" << endl;
+    cout << "Name    : Amrutha" << endl;
+    cout << "College : Maharaja Institute of Technology" << endl;
+    cout << "Day     : 1" << endl;
+    cout << "==================================" << endl;
+
+    cout << "*" << endl;
+    cout << "**" << endl;
+    cout << "***" << endl;
+
+    return 0;
+}
